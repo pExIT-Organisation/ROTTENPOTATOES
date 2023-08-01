4bb9b5ed01564091d04246d26dd4ce97fbcd526b
Artur-Ws <wysywysy97@gmail.com>
2023-08-01 21:08:57 +0200

    Add director field to Movie model

--- ROTTENPOTATOES/ranking/models.py
+++ ROTTENPOTATOES/ranking/models.py
@@ -8,3 +8,7 @@ class Movie(models.Model):
     image = models.CharField(max_length=4)
     rate = models.FloatField(max_length=4)
     description = models.CharField(max_length=300)
+    director = models.CharField(max_length=50, default=" ")
+
+    def __str__(self):
+        return f"{self.name} ({self.release_year})"
